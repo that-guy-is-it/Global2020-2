12b63d8f21f7dfb1de953da4a51456a9032acec8
that-guy-is-it <that.guy.is.it@gmail.com>
2020-02-01 15:09:18 -0500

    RIP in Peace

---
+++ Assets/Tyler's Scripts/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public GameObject menuManager;
+    public AudioMixer mixer;
+    public Slider musicSlider;
+    public Slider SFXSlider;
+
+
+    void Start()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("Music", 0.75f);
+        SFXSlider.value = PlayerPrefs.GetFloat("SFX", 0.75f);
+
+    }
+
+    public void SetMusicVolume()
+    {
+        float sliderValue = musicSlider.value;
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("Music", sliderValue);
+    }
+
+    public void SetSFXVolume()
+    {
+        float sliderValue = SFXSlider.value;
+        mixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("SFX", sliderValue);
+    }
+}
